24afdd4a9f991203ca778bed4b6311beb829acc8
taxuewwl <1210812591@qq.com>
2016-04-26 15:56:46 +0800

    modify some package defination and add package-info.java

---
+++ src/com/sunonline/web/service/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>title:业务层包说明类</p>
+ * <p>Copyright:Copyright(c)2016</p>
+ * <p>Company:太阳在线</p>
+ * <p>function:本包主要存放项目中的业务层接口定义</p>
+ * @author WuWenliang
+ * 
+ * @version $$Date:2016/04/26
+ *
+ */
+package com.sunonline.web.service;
