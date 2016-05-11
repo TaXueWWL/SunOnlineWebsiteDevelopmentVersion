aac47d9ed4598b8971eea67019f15ba5852757ad
taxuewwl <1210812591@qq.com>
2016-05-11 09:49:22 +0800

    添加代码注释

--- src/com/sunonline/web/utils/DBUtils.java
+++ src/com/sunonline/web/utils/DBUtils.java
@@ -8,28 +8,31 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.sunonline.web.global.DBGlobal;
+
 /**
  * @author 武文良
- * <p>date 2016.5.9</p>
- * 数据库工具类，获取/关闭数据库连接
+ *         <p>
+ *         date 2016.5.9
+ *         </p>
+ *         数据库工具类，获取/关闭数据库连接
  */
 public class DBUtils {
 
 	private Connection connection = null;
-	
+
 	/**
 	 * 获取数据库连接
+	 * 
 	 * @return connection
 	 */
 	public Connection getCon() {
 
 		try {
-			//加载驱动
+			// 加载驱动
 			Class.forName(DBGlobal.DB_DRIVER);
-			//建立连接
-			connection = DriverManager.getConnection(
-					DBGlobal.DB_URL, DBGlobal.DB_USERNAME, DBGlobal.DB_PASSWORD);
-			System.out.println( "connected==>" + connection.getMetaData().getURL());
+			// 建立连接
+			connection = DriverManager.getConnection(DBGlobal.DB_URL, DBGlobal.DB_USERNAME, DBGlobal.DB_PASSWORD);
+			System.out.println("connected==>" + connection.getMetaData().getURL());
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -37,15 +40,18 @@ public class DBUtils {
 		}
 		return connection;
 	}
+
 	/**
 	 * 关闭数据库连接
+	 * 
 	 * @param connection
 	 * @param statement
 	 * @param preparedStatement
 	 * @param resultSet
 	 * @throws SQLException
 	 */
-	public void closeCon(Connection connection, Statement statement, PreparedStatement preparedStatement, ResultSet resultSet) throws SQLException {
+	public void closeCon(Connection connection, Statement statement, PreparedStatement preparedStatement,
+			ResultSet resultSet) throws SQLException {
 		if (resultSet != null) {
 			resultSet.close();
 			System.out.println("关闭resultset");
